5d330a25f7f3a95a82f76787b5f3bc69d47d4daa
Bipin Vishwakarma <bipinvishwakarma256@gmail.com>
2024-06-15 19:29:58 +0530

    small change

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <div id='home' className='hero'>
       <img src={profile_img} alt="" className='profile-img' />
       <h1><span>I'm Bipin Vishwakarma,</span> frontend developer based in India.</h1>
-      <p>I am a frontend developer from California, USA with 10 years of experience in multiple companies like Microsoft, Tesla and Apple.</p>
+      <p>I am a frontend developer with 1.5years of experience at Hipaas Inc.</p>
       <div className="hero-action">
         <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
         <div className="hero-resume"><a href='https://drive.google.com/file/d/1qnH9lw48_XU9BWF7OeddQ2chMzn6lGCi/view?usp=drive_link' target="_blank" rel="noopener noreferrer" style={{textDecoration:"none", color:"white"}}>My resume</a></div>
